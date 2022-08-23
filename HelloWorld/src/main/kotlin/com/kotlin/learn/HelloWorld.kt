8140ec0e2aea93d9e51882935bdd8a4e06d14387
ashutoshk2206 <ashutoshk2206@yahoo.com>
2022-08-23 15:21:55 +0530

    learning

--- HelloWorld/src/main/kotlin/com/kotlin/learn/HelloWorld.kt
+++ HelloWorld/src/main/kotlin/com/kotlin/learn/HelloWorld.kt
@@ -2,4 +2,24 @@ package com.kotlin.learn
 
 fun main(args: Array<String>) {
     println("Hello, World!")
+
+    val hello1 = "Hello"
+    val hello2 = "Hello"
+
+    println(hello1 === hello2)
+    println(hello1 == hello2)
+    println(hello1.plus(hello2))
+
+    var num = 2988
+
+    val any: Any = "The Any type is the root of Kotlin class hierarchy"
+    if (any is String) {
+        println(any.uppercase())
+    }
+
+    println(""" 1   1
+                1  11
+                1 111
+                11111
+    """.trimMargin("1"))
 }
